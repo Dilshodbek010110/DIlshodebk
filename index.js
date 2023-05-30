4f662fdbed8d2cc48b22d1576c5bba92f5f45473
Dilshodbek <dilshodbekk2005@iclod.com>
2023-05-30 17:00:15 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+let name = "Alex"
+let money = 10000
+let account = 7777
+
+let first = prompt(`What is your name ?`).toLowerCase().trim()
+
+if (first = name){
+alert("welcome alex")
+code = prompt(`Номер счета?`).trim()
+if (code == account) {
+
+dollar = prompt(`Сколько обналичить?`).trim()
+if (dollar <= money){
+alert(`All good, сняли ${dollar}, осталось ${money - dollar}`)
+} 
+else{
+alert("недостаточно средств")
+}
+}
+else{
+alert("пользователь не найдено")
+}
+} 
+
+else{
+    console.log(
+        alert("пользователь не найдено")
+    );
+}
+
+    
